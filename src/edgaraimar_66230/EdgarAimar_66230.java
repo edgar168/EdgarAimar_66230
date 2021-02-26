e539776f2572f2d1e7b50a8306981a28c3b1739b
EDGAR AIMAR <EDGAR AIMAR@DESKTOP-GRP7BKA>
2021-02-25 20:24:38 -0600

    Ejemplo con mi nombre Edgar

---
+++ src/edgaraimar_66230/EdgarAimar_66230.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edgaraimar_66230;
+
+/**
+ *
+ * @author EDGAR AIMAR
+ */
+public class EdgarAimar_66230 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+System.out.println("Mi nombre es Edgar Aimar Cach Tamay.");
+    }
+
+}
